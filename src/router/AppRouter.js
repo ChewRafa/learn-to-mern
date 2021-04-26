a241e73f641e7bec703d582eb9c0a0d8435e28ce
Rafael Ramírez <yphar89@gmail.com>
2021-04-26 13:02:48 -0500

    retain entered data across steps

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,21 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import FirstStep from '../components/FirstStep';
 import SecondStep from '../components/SecondStep';
 import ThirdStep from '../components/ThirdStep';
 import Header from '../components/Header';
 
-const AppRouter = () => (
-  <BrowserRouter>
-	<div className="container">
-	<Header/>
-	<Switch>
-		<Route component={FirstStep} path="/" exact={true} />
-		<Route component={SecondStep} path="/second" exact={true} />
-		<Route component={ThirdStep} path="/third" exact={true} />
-	</Switch>
-	</div>
-</BrowserRouter>
-);
+
+
+const AppRouter = () => {
+
+	const [user, setUser] = useState({});
+
+	const updateUser = (data) => {
+		setUser((prevUser) => ({ ...prevUser, ...data }));
+	};
+
+	const resetUser = () => {
+		setUser({});
+	};
+
+	return (
+		<BrowserRouter>
+			<div className="container">
+				<Header />
+				<Switch>
+					<Route
+						render={(props) => (<FirstStep {...props} user={user} updateUser={updateUser} />
+						)}
+						path="/"
+						exact={true}
+					/>
+					<Route
+						render={(props) => (
+							<SecondStep {...props} user={user} updateUser={updateUser} />
+						)}
+						path="/second"
+						exact={true}
+					/>
+					<Route
+						render={(props) => (
+							<ThirdStep {...props} user={user} />
+						)}
+						path="/third"
+						exact={true}
+					/>
+				</Switch>
+			</div>
+		</BrowserRouter>
+	);
+};
 
 export default AppRouter;

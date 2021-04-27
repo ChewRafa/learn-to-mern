c130db4e45040af8a7daf91753f5067bfeab0802
Rafael Ramírez <yphar89@gmail.com>
2021-04-27 12:52:49 -0500

    enable user router

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const userRouter = require('./routers/user');
 require('./db');
 
 const app = express();
 const PORT = process.env.PORT || 3030;
 
+app.use(express.json());
+
+//allows to make API requests
+app.use(userRouter);
+
 app.get('/', (req, res) => {
   res.send('<h2>This is from index.js file</h2>');
 });
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });

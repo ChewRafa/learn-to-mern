bef958cfb6f364f7eac75a966a10fb64abb77752
Rafael Ramírez <yphar89@gmail.com>
2021-05-11 15:00:28 -0500

    production build enable

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const userRouter = require('./routers/user');
 const cors = require('cors');
+const path = require('path');
 require('./db');
 
 const app = express();
 const PORT = process.env.PORT || 3030;
 
+app.use(express.static(path.join(__dirname, '..', 'build')));
+
 app.use(express.json());
 
 //enables same-site requests
-app.use(cors());
+// app.use(cors());
 //allows to make API requests
 app.use(userRouter);
 

45a6c3475c2d1bd9871030fce1d68ce8f83ef2be
Rafael Ramírez <yphar89@gmail.com>
2021-05-12 14:12:27 -0500

    production build fixes

--- server/index.js
+++ server/index.js
@@ -1,25 +1,23 @@
+const path = require('path');
 const express = require('express');
 const userRouter = require('./routers/user');
-const cors = require('cors');
-const path = require('path');
 require('./db');
 
 const app = express();
 const PORT = process.env.PORT || 3030;
 
 app.use(express.static(path.join(__dirname, '..', 'build')));
-
 app.use(express.json());
-
-//enables same-site requests
-// app.use(cors());
-//allows to make API requests
 app.use(userRouter);
 
 app.get('/', (req, res) => {
   res.send('<h2>This is from index.js file</h2>');
 });
 
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
+
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+  console.log(`server started on port ${PORT}`);
 });
